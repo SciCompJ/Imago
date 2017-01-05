aba50ec92a96510be4f8ec6554f5351844b9a9f8
dlegland <david.legland@inra.fr>
2017-01-05 15:41:03 +0100

    update MergeChannelImagesAction using streams

--- src/main/java/imago/gui/action/image/MergeChannelImagesAction.java
+++ src/main/java/imago/gui/action/image/MergeChannelImagesAction.java
@@ -5,15 +5,11 @@ package imago.gui.action.image;
 
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 
-import imago.app.ImagoDoc;
+import imago.app.ImagoApp;
 import imago.gui.GenericDialog;
 import imago.gui.ImagoAction;
-import imago.gui.ImagoDocViewer;
 import imago.gui.ImagoFrame;
-import net.sci.array.Array;
 import net.sci.array.data.UInt8Array;
 import net.sci.array.data.color.RGB8Array;
 import net.sci.image.Image;
@@ -42,51 +38,50 @@ public class MergeChannelImagesAction extends ImagoAction
 	 * java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
 	 */
 	@Override
-	public void actionPerformed(ActionEvent arg0)
+	public void actionPerformed(ActionEvent evt)
 	{
-		Collection<ImagoDocViewer> viewers = this.gui.getDocumentViewers();
-		HashMap<String, ImagoDoc> nameToDocMap = new HashMap<String, ImagoDoc>();
+		// get pointer to app for finding images 
+		ImagoApp app = this.gui.getAppli();
 		
-		ArrayList<String> imageNames = new ArrayList<String>(viewers.size());
-		for (ImagoDocViewer viewer : viewers)
-		{
-			ImagoDoc doc = viewer.getDocument(); 
-			if (doc == null) continue;
-			Image image = doc.getImage();
-			if (image == null) continue;
-			
-			// restrict the image selection to instance of UInt8 arrays
-			Array<?> data = image.getData();
-			if (data instanceof UInt8Array)
-			{
-				imageNames.add(doc.getName());
-				nameToDocMap.put(doc.getName(), doc);
-			}
-		}
-		
+		// collect the names of documents containing UInt8 images
+		ArrayList<String> imageNames = new ArrayList<>();
+		app.getDocuments().stream()
+			.filter(doc -> {
+				Image img = doc.getImage();
+				if (img == null) return false;
+				return img.getData() instanceof UInt8Array;
+			})
+			.forEach(doc -> imageNames.add(doc.getName()));
+
+		// do not continue if no UInt8 image is loaded
 		if (imageNames.size() == 0)
 		{
 			return;
 		}
 		
+		// Convert image name list to String array
 		String[] imageNameArray = imageNames.toArray(new String[]{});
 		String firstImageName = imageNameArray[0];
 				
+		// Create Dialog for choosing image names
 		GenericDialog dialog = new GenericDialog("Merge Channels");
 		dialog.addChoice("Red channel:", imageNameArray, firstImageName);
 		dialog.addChoice("Green channel:", imageNameArray, firstImageName);
 		dialog.addChoice("Blue channel:", imageNameArray, firstImageName);
 
+		// Display dialog and wait for OK or Cancel
 		dialog.showDialog();
 		if (dialog.wasCanceled())
 		{
 			return;
 		}
 		
-		Image redChannelImage 	= nameToDocMap.get(dialog.getNextChoice()).getImage();
-		Image greenChannelImage = nameToDocMap.get(dialog.getNextChoice()).getImage();
-		Image blueChannelImage 	= nameToDocMap.get(dialog.getNextChoice()).getImage();
+		// Retrieve images from document names
+		Image redChannelImage 	= app.getDocumentFromName(dialog.getNextChoice()).getImage();
+		Image greenChannelImage = app.getDocumentFromName(dialog.getNextChoice()).getImage();
+		Image blueChannelImage 	= app.getDocumentFromName(dialog.getNextChoice()).getImage();
 		
+		// extract arrays containing image data
 		UInt8Array redChannel 	= (UInt8Array) redChannelImage.getData();
 		UInt8Array greenChannel = (UInt8Array) greenChannelImage.getData();
 		UInt8Array blueChannel 	= (UInt8Array) blueChannelImage.getData();
