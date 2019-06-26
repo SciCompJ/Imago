2dd549d6e967953300498cd3213a7c0cd267b30d
dlegland <david.legland@inra.fr>
2019-06-26 14:38:36 +0200

    refactor DisplayCurrentValueTool

--- src/main/java/imago/gui/tool/DisplayCurrentValueTool.java
+++ src/main/java/imago/gui/tool/DisplayCurrentValueTool.java
@@ -15,8 +15,6 @@ import java.awt.event.MouseEvent;
 import java.util.Locale;
 
 import net.sci.array.Array;
-import net.sci.array.Array2D;
-import net.sci.array.Array3D;
 import net.sci.array.color.RGB16Array;
 import net.sci.array.color.RGB8Array;
 import net.sci.array.scalar.IntArray;
@@ -69,16 +67,16 @@ public class DisplayCurrentValueTool extends ImagoTool
 		// Extract image
 		ImageViewer imageView = viewer.getImageView();
 		Image image = imageView.getImage();
-		Array<?> img = image.getData();
+		Array<?> array = image.getData();
 
-		// Coordinate of mouse cursor in panel corodinates
+		// Coordinate of mouse cursor in panel coordinates
 		ImageDisplay display = (ImageDisplay) evt.getSource();
 		Point point = new Point(evt.getX(), evt.getY());
 		
 		// convert coordinates to image reference system
-		Point2D pos = display.displayToImage(point);
-		double x = pos.getX();
-		double y = pos.getY();
+		Point2D pos2d = display.displayToImage(point);
+		double x = pos2d.getX();
+		double y = pos2d.getY();
 
 		// convert to integer indices
         int indx = (int) Math.round(x);
@@ -91,89 +89,99 @@ public class DisplayCurrentValueTool extends ImagoTool
         if (indy < 0 || indy > image.getSize(1) - 1)
             return;
 
-		// Create string for representing position
+		// String patterns for representing position
         String floatFormat = "%.4g";
 		String posString = "";
-		if (img instanceof Array2D)
+		
+        // create the position array
+		int nd = array.dimensionality();
+        int[] pos = new int[nd];
+
+        // populate the position array depending on array dimensionality
+        pos[0] = indx;
+        pos[1] = indy;
+
+        if (nd == 2)
 		{
 //            String format = "pos=(" + floatFormat + ", " + floatFormat + ")";
             String format = String.format("pos=(%s, %s)", floatFormat, floatFormat);
             posString = String.format(Locale.ENGLISH, format, x, y);
-
 		}
-		else if (img instanceof Array3D)
+		else if (nd == 3)
 		{
 		    if (imageView instanceof StackSliceViewer)
 		    {
     			indz = ((StackSliceViewer) imageView).getSliceIndex();
+                pos[2] = indz;
+
 //    			String format = "pos=(%g, %g, %d)";
     			String format = String.format("pos=(%s, %s, %s)", floatFormat, floatFormat, "%d");
     			posString = String.format(Locale.ENGLISH, format, x, y, indz);
 		    }
 		}
 
-		// create the position array
-		int[] inds = new int[img.dimensionality()];
-		inds[0] = indx;
-		inds[1] = indy;
-		if (img instanceof Array3D)
-		{
-			inds[2] = indz;
-		}
-
 		// Create string for representing image value
-		String valueString = null;
-		if (img instanceof ScalarArray)
-		{
-			if (img instanceof IntArray)
-			{
-				// Case of integer scalar images
-				int val = ((IntArray<?>) img).getInt(inds);
-				valueString = String.format("val=%d", val);
-
-			} 
-			else
-			{
-				// Case of floating-point scalar images
-				double val = ((ScalarArray<?>) img).getValue(inds);
-				valueString = String.format(Locale.US, "val=%g", val);
-			}
-
-		} 
-        else if (img instanceof RGB8Array)
-        {
-            // Case of RGB color images
-            int[] rgb = ((RGB8Array) img).get(inds).getSamples();
-            valueString = String.format("rgb=[%d,%d,%d]", rgb[0], rgb[1], rgb[2]);
-
-        } 
-        else if (img instanceof RGB16Array)
-        {
-            // Case of RGB color images
-            int[] rgb = ((RGB16Array) img).get(inds).getSamples();
-            valueString = String.format("rgb=[%d,%d,%d]", rgb[0], rgb[1], rgb[2]);
-
-        } 
-		else if (img instanceof VectorArray)
-		{
-			// in case of Vector array, compute the norm of the pixel for display
-			double[] values = ((VectorArray<?>) img).getValues(inds);
-			double norm = 0;
-			for (int c = 0; c < values.length; c++)
-				norm += values[c] * values[c];
-			norm = Math.sqrt(norm);
-			valueString = String.format(Locale.US, "norm=%g", norm);
-		} 
-		else
-		{
-			throw new IllegalArgumentException(
-					"Unable to manage image of class " + img.getClass());
-		}
+		String valueString = createValueString(array, pos);
 
 		// Concatenate the information and update status bar
 		StatusBar statusBar = viewer.getStatusBar();
 		String format = "%1$s %2$s";
 		String label = String.format(format, posString, valueString);
 		statusBar.setCursorLabel(label);
+	}
+
+	/**
+     * Creates string for representing image value at a given position
+     *
+     * @param array
+     *            the array containing image values
+     * @param pos
+     *            the position
+     * @return a string representing the value
+     */
+	private static final String createValueString(Array<?> array, int[] pos)
+	{
+        if (array instanceof ScalarArray)
+        {
+            if (array instanceof IntArray)
+            {
+                // Case of integer scalar images
+                int val = ((IntArray<?>) array).getInt(pos);
+                return String.format("val=%d", val);
+            } 
+            else
+            {
+                // Case of floating-point scalar images
+                double val = ((ScalarArray<?>) array).getValue(pos);
+                return String.format(Locale.US, "val=%g", val);
+            }
+        } 
+        else if (array instanceof RGB8Array)
+        {
+            // Case of RGB color images
+            int[] rgb = ((RGB8Array) array).get(pos).getSamples();
+            return String.format("rgb=[%d,%d,%d]", rgb[0], rgb[1], rgb[2]);
+        } 
+        else if (array instanceof RGB16Array)
+        {
+            // Case of RGB color images
+            int[] rgb = ((RGB16Array) array).get(pos).getSamples();
+            return String.format("rgb=[%d,%d,%d]", rgb[0], rgb[1], rgb[2]);
+        } 
+        else if (array instanceof VectorArray)
+        {
+            // in case of Vector array, compute the norm of the pixel for display
+            double[] values = ((VectorArray<?>) array).getValues(pos);
+            double norm = 0;
+            for (int c = 0; c < values.length; c++)
+                norm += values[c] * values[c];
+            norm = Math.sqrt(norm);
+            return String.format(Locale.US, "norm=%g", norm);
+        } 
+        else
+        {
+            throw new IllegalArgumentException(
+                    "Unable to manage image of class " + array.getClass());
+        }
 	}
 }
