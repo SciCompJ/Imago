532c345d64dc5ab3d5f45efc0cdc4de4b32446ad
dlegland <david.legland@inra.fr>
2019-09-05 16:00:24 +0200

    centroid now uses Region analysis interface

--- src/main/java/imago/plugin/image/analyze/LabelImageCentroids.java
+++ src/main/java/imago/plugin/image/analyze/LabelImageCentroids.java
@@ -3,6 +3,8 @@
  */
 package imago.plugin.image.analyze;
 
+import java.util.Map;
+
 import imago.app.ImagoDoc;
 import imago.app.shape.ImagoShape;
 import imago.gui.ImagoDocViewer;
@@ -10,13 +12,13 @@ import imago.gui.ImagoFrame;
 import imago.gui.ImagoTableFrame;
 import imago.gui.Plugin;
 import net.sci.array.Array;
-import net.sci.array.scalar.IntArray2D;
+import net.sci.array.scalar.IntArray;
 import net.sci.array.scalar.IntArray3D;
 import net.sci.geom.geom2d.Point2D;
 import net.sci.geom.geom3d.Point3D;
 import net.sci.image.Image;
-import net.sci.image.analyze.RegionAnalysis2D;
 import net.sci.image.analyze.RegionAnalysis3D;
+import net.sci.image.analyze.region2d.Centroid;
 import net.sci.image.morphology.LabelImages;
 import net.sci.table.Table;
 
@@ -49,8 +51,7 @@ public class LabelImageCentroids implements Plugin
         Image image = doc.getImage();
         if (!image.isLabelImage())
         {
-            System.out.println("Requires label image as input");
-            return;
+            throw new IllegalArgumentException("Requires label image as input");
         }
         
         Array<?> array = image.getData();
@@ -59,22 +60,31 @@ public class LabelImageCentroids implements Plugin
         if (nd == 2)
         {
             // check input data type
-            if (!(array instanceof IntArray2D))
+            if (!(array instanceof IntArray))
             {
-                System.out.println("Requires a planar array of int");
-                return;
+                throw new IllegalArgumentException("Requires an array of int");
             }
     
             // Extract centroids as an array of points
-            IntArray2D<?> array2d = (IntArray2D<?>) array;
-            int[] labels = LabelImages.findAllLabels(array2d); 
-            Point2D[] centroids = RegionAnalysis2D.centroids(array2d, labels);
+//            IntArray2D<?> array2d = (IntArray2D<?>) array;
+//            int[] labels = LabelImages.findAllLabels(array2d); 
+//            Point2D[] centroids = RegionAnalysis2D.centroids(array2d, labels);
              
+            Centroid algo = new Centroid();
+            Map<Integer, Point2D> centroids = algo.analyzeRegions(image);
             // add to the document
-            for (int i = 0; i < centroids.length; i++)
+            for (Point2D centroid : centroids.values())
             {
-                doc.addShape(new ImagoShape(centroids[i]));
+                doc.addShape(new ImagoShape(centroid));
             }
+
+            Table table = algo.createTable(centroids);
+            frame.getGui().addFrame(new ImagoTableFrame(frame, table));
+//            // add to the document
+//            for (int i = 0; i < centroids.length; i++)
+//            {
+//                doc.addShape(new ImagoShape(centroids[i]));
+//            }
             frame.repaint();
         }
         else if (nd == 3)
