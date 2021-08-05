8e63aee227bbba1dc9be65ddd2b4acb43b5f7ff9
dlegland <david.legland@inrae.fr>
2021-08-05 08:56:53 +0200

    rebase cleanup

--- src/main/java/imago/plugin/image/edit/ImageSelectionToMask.java
+++ src/main/java/imago/plugin/image/edit/ImageSelectionToMask.java
@@ -5,11 +5,9 @@ package imago.plugin.image.edit;
 
 import imago.app.ImageHandle;
 import imago.gui.FramePlugin;
-import imago.gui.ImageFrame;
+import imago.gui.frames.ImageFrame;
 import imago.gui.ImageViewer;
 import imago.gui.ImagoFrame;
-import imago.gui.frames.ImageFrame;
-import imago.gui.FramePlugin;
 import imago.gui.viewer.PlanarImageViewer;
 import net.sci.array.Array;
 import net.sci.array.scalar.BinaryArray2D;
