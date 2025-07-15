ee5cba72e747c5cfbf56dba0571011aefa2d5f4d
dlegland <david.legland@inrae.fr>
2025-07-15 19:04:51 +0200

    FoldTableToImage can now create Label Images

--- src/main/java/imago/table/plugins/edit/FoldTableToImage.java
+++ src/main/java/imago/table/plugins/edit/FoldTableToImage.java
@@ -10,7 +10,13 @@ import imago.image.ImageFrame;
 import imago.table.TableFrame;
 import imago.table.plugins.TableFramePlugin;
 import net.sci.array.numeric.Float32Array;
+import net.sci.array.numeric.Int16Array;
+import net.sci.array.numeric.ScalarArray;
+import net.sci.array.numeric.UInt8Array;
 import net.sci.image.Image;
+import net.sci.image.ImageType;
+import net.sci.table.Column;
+import net.sci.table.IntegerColumn;
 import net.sci.table.Table;
 
 /**
@@ -34,11 +40,15 @@ public class FoldTableToImage implements TableFramePlugin
         Table table = ((TableFrame) frame).getTable();
         int nr = table.rowCount();
 
+        // peek the first column
+        Column col = table.column(0);
+        
         // TODO: check table is all numeric
 
         int sizeX;
         int sizeY;
         int sizeZ;
+        boolean labelMap = false;
 
         // loop until valid values are entered
         while (true)
@@ -48,7 +58,10 @@ public class FoldTableToImage implements TableFramePlugin
             dlg.addNumericField("Image Size X: ", nr, 0);
             dlg.addNumericField("Image Size Y: ", 1, 0);
             dlg.addNumericField("Image Size Z: ", 1, 0);
-            
+            if (col instanceof IntegerColumn)
+            {
+                dlg.addCheckBox("Create Label Image", false);
+            }
             // wait for user validation
             dlg.showDialog();
             if (dlg.wasCanceled())
@@ -60,6 +73,10 @@ public class FoldTableToImage implements TableFramePlugin
             sizeX = (int) dlg.getNextNumber();
             sizeY = (int) dlg.getNextNumber();
             sizeZ = (int) dlg.getNextNumber();
+            if (col instanceof IntegerColumn)
+            {
+                labelMap = dlg.getNextBoolean();
+            }
             
             // check input validity
             if (sizeX * sizeY * sizeZ == nr)
@@ -72,28 +89,56 @@ public class FoldTableToImage implements TableFramePlugin
             }
         }
         
-        // fill result array with first column content
-        Float32Array res;
-        if (sizeZ == 1)
+        // determines size of output image
+        int[] dims = (sizeZ == 1) ? new int[] {sizeX, sizeY} : new int[] {sizeX, sizeY, sizeZ};  
+        
+        // choose array factory depending on column type
+        ScalarArray.Factory<?> factory = null;
+        if (col instanceof IntegerColumn)
         {
-            res = Float32Array.create(sizeX, sizeY);
+            if (labelMap)
+            {
+                factory = UInt8Array.defaultFactory;
+            }
+            else
+            {
+                factory = Int16Array.defaultFactory;
+            }
         }
         else
         {
-            res = Float32Array.create(sizeX, sizeY, sizeZ);
+            factory = Float32Array.defaultFactory;
         }
         
-        Float32Array.Iterator iter = res.iterator();
-        for (int r = 0; r < nr; r++)
+        // fill result array with first column content
+        ScalarArray<?> res = factory.create(dims);
+        
+        Image image;
+        if (labelMap)
         {
-            iter.setNextValue(table.getValue(r, 0));
+            ScalarArray.Iterator<?> iter = res.iterator();
+            int labelMax = 0;
+            for (int r = 0; r < nr; r++)
+            {
+                int value = (int) col.getValue(r) + 1;
+                labelMax = Math.max(value, labelMax);
+                iter.setNextValue(value);
+            }
+            image = new Image(res, ImageType.LABEL);
+            image.getDisplaySettings().setDisplayRange(new double[] {0, labelMax});
         }
-        
-        Image image = new Image(res);
+        else
+        {
+            ScalarArray.Iterator<?> iter = res.iterator();
+            for (int r = 0; r < nr; r++)
+            {
+                iter.setNextValue(table.getValue(r, 0));
+            }
+            image = new Image(res);
+        }
         image.setName(table.getName() + "-fold");
-        
+
         // add the image document to GUI
         ImageFrame.create(image, frame);
     }
-
 }
