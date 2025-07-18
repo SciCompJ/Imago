12040e5a38d4c530bf1e7114b0b1989f97957995
dlegland <david.legland@inrae.fr>
2025-07-18 20:06:20 +0200

    ImageMeanValue can now process vector images

--- src/main/java/imago/plugin/image/analyze/ImageMeanValue.java
+++ src/main/java/imago/plugin/image/analyze/ImageMeanValue.java
@@ -7,10 +7,13 @@ import imago.gui.ImagoFrame;
 import imago.gui.image.ImageFrame;
 import imago.gui.image.ImageViewer;
 import imago.gui.image.PlanarImageViewer;
+import imago.gui.table.TableFrame;
 import imago.gui.FramePlugin;
 import net.sci.array.Array;
 import net.sci.array.numeric.ScalarArray;
 import net.sci.array.numeric.ScalarArray2D;
+import net.sci.array.numeric.VectorArray;
+import net.sci.array.numeric.VectorArray2D;
 import net.sci.geom.geom2d.Geometry2D;
 import net.sci.geom.geom2d.Point2D;
 import net.sci.geom.geom2d.polygon.DefaultPolygon2D;
@@ -18,13 +21,17 @@ import net.sci.image.Image;
 import net.sci.table.NumericTable;
 
 /**
- * Compute mean value within the whole image or within the current region selection.
+ * Compute mean value within the whole image or within the current region
+ * selection.
  * 
  * @author David Legland
  *
  */
 public class ImageMeanValue implements FramePlugin
 {
+    /**
+     * Default empty constructor.
+     */
     public ImageMeanValue()
     {
     }
@@ -42,8 +49,8 @@ public class ImageMeanValue implements FramePlugin
         if (!(frame instanceof ImageFrame))
             return;
         ImageFrame iframe = (ImageFrame) frame;
-        Image meta = iframe.getImageHandle().getImage();
-        Array<?> array = meta.getData();
+        Image image = iframe.getImageHandle().getImage();
+        Array<?> array = image.getData();
         
         ImageViewer viewer = iframe.getImageViewer();
         if (!(viewer instanceof PlanarImageViewer))
@@ -62,8 +69,11 @@ public class ImageMeanValue implements FramePlugin
         
         DefaultPolygon2D poly = (DefaultPolygon2D) selection;
         
-        // manage clockwise and counter-clockwise polygons
-        boolean clockWise = poly.signedArea() < 0;
+        // ensure counter-clockwise orientation of polygon
+        if (poly.signedArea() < 0)
+        {
+            poly = poly.complement();
+        }
         
         NumericTable table = null;
         if (array instanceof ScalarArray)
@@ -80,7 +90,7 @@ public class ImageMeanValue implements FramePlugin
             {
                 for (int x = 0; x < sizeX; x++)
                 {
-                    if (poly.contains(new Point2D(x, y)) ^ clockWise)
+                    if (poly.contains(new Point2D(x, y)))
                     {
                         sum += array2d.getValue(x, y);
                         count++;
@@ -90,27 +100,59 @@ public class ImageMeanValue implements FramePlugin
 
             double meanValue = count == 0 ? Double.NaN : sum / count;
             table = NumericTable.create(1, 3);
-            table.setColumnNames(new String[] { "Mean", "Sum", "Count" });
             table.setValue(0, 0, meanValue);
             table.setValue(0, 1, sum);
             table.setValue(0, 2, count);
             
-            table.show();
+            table.setColumnNames(new String[] { "Mean", "Sum", "Count" });
+            table.setRowAxis(image.getCalibration().getChannelAxis());
+            table.setName(image.getName() + "-roi");
+            TableFrame.create(table, frame);
         }
-//        else if (array instanceof RGB8Array2D)
-//        {
-//            table = colorProfile((RGB8Array2D) array, p1, p2, n);
-//            plotRGB8LineProfile(table);
-//        }
-//        else if (array instanceof VectorArray2D)
-//        {
-//            ScalarArray2D<?> normImage = ((VectorArray2D<?>) array).norm();
-//            table = intensityProfile(normImage, p1, p2, n);
-//            plotIntensityProfile(table);
-//        }
+        else if (array instanceof VectorArray2D)
+        {
+            VectorArray2D<?,?> array2d = VectorArray2D.wrap((VectorArray<?,?>) array);
+            
+            int sizeX = array2d.size(0);
+            int sizeY = array2d.size(1);
+            int nChannels = array2d.channelCount();
+            
+            double[] sums = new double[nChannels];
+            int[] counts = new int[nChannels];
+            
+            for (int y = 0; y < sizeY; y++)
+            {
+                for (int x = 0; x < sizeX; x++)
+                {
+                    if (poly.contains(new Point2D(x, y)))
+                    {
+                        for (int c = 0; c < nChannels; c++)
+                        {
+                            sums[c] += array2d.getValue(x, y, c);
+                            counts[c] ++;
+                        }
+                    }
+                }
+            }
+
+            table = NumericTable.create(nChannels, 3);
+            
+            for (int c = 0; c < nChannels; c++)
+            {
+                double meanValue = counts[c] == 0 ? Double.NaN : sums[c] / counts[c];
+                table.setValue(c, 0, meanValue);
+                table.setValue(c, 1, sums[c]);
+                table.setValue(c, 2, counts[c]);
+            }
+            
+            table.setColumnNames(new String[] { "Mean", "Sum", "Count" });
+            table.setRowAxis(image.getCalibration().getChannelAxis());
+            table.setName(image.getName() + "-roi");
+            TableFrame.create(table, frame);
+        }
         else
             throw new RuntimeException(
-                    "Can not process image from class: " + array.getClass().getName());
+                    "Can not process image with array class: " + array.getClass().getName());
         
     }
 }
