af2c158ade94b3327add4a61619987ec00961d0b
dlegland <david.legland@inrae.fr>
2025-10-01 20:52:20 +0200

    change title of empty frame

--- src/main/java/imago/gui/frames/ImagoEmptyFrame.java
+++ src/main/java/imago/gui/frames/ImagoEmptyFrame.java
@@ -15,35 +15,36 @@ import imago.gui.ImagoGui;
  * @author David Legland
  *
  */
-public class ImagoEmptyFrame extends ImagoFrame {
+public class ImagoEmptyFrame extends ImagoFrame
+{
+    // ===================================================================
+    // Static class variables
 
-	// ===================================================================
-	// Static class variables
-	
-	// ===================================================================
-	// Constructor
+    // ===================================================================
+    // Constructor
 
-	public ImagoEmptyFrame(ImagoGui gui) 
-	{
-		super(gui, "Image Frame");
-		
+    public ImagoEmptyFrame(ImagoGui gui)
+    {
+        super(gui, "Imago");
+
         GuiBuilder builder = new GuiBuilder(this);
         builder.createMenuBar();
-		
-		putFrameTopLeft();
-	}
-	
-	/** 
-	 * Set up frame size depending on screen size
-	 */
-	private void putFrameTopLeft()
-	{
-		this.jFrame.setMinimumSize(new Dimension(300, 0));
-		this.jFrame.pack();
-		
-		// set up frame position in the upper left corner
-		int posX = 300;
-		int posY = 50;
-		this.jFrame.setLocation(posX, posY);
-	}
+
+        initializePosition();
+    }
+
+    /**
+     * Set up frame position on top-left of screen, with a position depending on
+     * screen size.
+     */
+    private void initializePosition()
+    {
+        this.jFrame.setMinimumSize(new Dimension(300, 0));
+        this.jFrame.pack();
+
+        // set up frame position in the upper left corner
+        int posX = 300;
+        int posY = 50;
+        this.jFrame.setLocation(posX, posY);
+    }
 }
