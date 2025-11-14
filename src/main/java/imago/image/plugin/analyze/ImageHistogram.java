021486eb871d9b586406034e5dff05dc3062ebd5
dlegland <david.legland@inrae.fr>
2025-11-14 19:40:59 +0100

    refactor ImageHistogram plugin class to rely on "ImageHistograms" utility class

--- src/main/java/imago/image/plugin/analyze/ImageHistogram.java
+++ src/main/java/imago/image/plugin/analyze/ImageHistogram.java
@@ -29,12 +29,14 @@ import imago.gui.FramePlugin;
 import imago.gui.ImagoFrame;
 import imago.image.ImageFrame;
 import net.sci.array.Array;
-import net.sci.array.Histograms;
 import net.sci.array.color.RGB16Array;
 import net.sci.array.color.RGB8Array;
 import net.sci.array.numeric.ScalarArray;
 import net.sci.array.numeric.UInt8Array;
+import net.sci.array.numeric.VectorArray;
+import net.sci.array.numeric.process.Histogram;
 import net.sci.image.Image;
+import net.sci.image.analyze.ImageHistograms;
 
 /**
  * @author David Legland
@@ -62,10 +64,10 @@ public class ImageHistogram implements FramePlugin
 		Image image = iframe.getImageHandle().getImage();
 		
 		// Create the class containing histogram data
-		Histogram histo = new Histogram(image);
+		HistogramSettings settings = new HistogramSettings(image);
 		
         String name = "Histogram of " + image.getName();
-		ImageHistogramDisplayFrame histoFrame = new ImageHistogramDisplayFrame(frame, name, histo);
+		ImageHistogramDisplayFrame histoFrame = new ImageHistogramDisplayFrame(frame, name, image, settings);
 		
         // Schedule a job for the event-dispatching thread:
         // creating and showing this application's GUI.
@@ -94,43 +96,25 @@ public class ImageHistogram implements FramePlugin
     /**
      * The histogram data.
      */
-    class Histogram
+    class HistogramSettings
     {
-        /** the reference image, for retrieving meta-data and eventually recomputing the histogram */
-        Image refImage;
-        
         /**
          * The range of values of image. Computed once at initialization.
          */
         double[] imageValueRange;
         
-        /** The position of histogram bins. */
-        double[] xData;
-        
-        /**
-         * The frequency count for each channel. On array per channel, the same
-         * size as xData.
-         */
-        int[][] yData;
-        
         double minValue = 0.0;
         double maxValue = 255.0;
         int nBins = 256;
         
-        /** The name of each channel. Same size as yData. */
-        String[] channelNames;
-
-        public Histogram(Image image)
+        public HistogramSettings(Image image)
         {
-            this.refImage = image;
-            
-            computeDefaultValueRange();
-            update();
+            computeDefaultValueRange(image);
         }
         
-        private void computeDefaultValueRange()
+        private void computeDefaultValueRange(Image image)
         {
-            Array<?> array = this.refImage.getData();
+            Array<?> array = image.getData();
             if (!(array instanceof ScalarArray)) return;
             
             if (array instanceof UInt8Array)
@@ -145,109 +129,6 @@ public class ImageHistogram implements FramePlugin
             this.maxValue = imageValueRange[1];
         }
         
-        /**
-         * Updates histogram data based on the inner bounds and bin number. Image
-         * values will be iterated.
-         */
-        public void update()
-        {
-            Array<?> array = this.refImage.getData();
-            
-            if (array instanceof UInt8Array)            
-            {
-                computeHistogramData_UInt8((UInt8Array) array);
-            }
-            else if (array instanceof RGB8Array)            
-            {
-                computeHistogramData_RGB8((RGB8Array) array);
-            }
-            else if (array instanceof RGB16Array)            
-            {
-                computeHistogramData_RGB16((RGB16Array) array);
-            }
-            else if (array instanceof ScalarArray)            
-            {
-                computeHistogramData_Scalar((ScalarArray<?>) array);
-            }
-            else
-            {
-                throw new RuntimeException("Unable to compute histogram for array class: " + array.getClass());
-            }
-        }
-        
-        private void computeHistogramData_UInt8(UInt8Array array)
-        {
-            this.xData = new double[256];
-            for (int i = 0; i < 256; i++)
-            {
-                xData[i] = i;
-            }
-            
-            // allocate memory for result
-            this.yData = new int[1][];
-            this.yData[0] = Histograms.histogramUInt8(array);
-            
-            this.channelNames = new String[] {"Intensity"};
-        }
-        
-        private void computeHistogramData_Scalar(ScalarArray<?> array)
-        {
-            // compute the sizeX of an individual bin
-            double binWidth = (maxValue - minValue) / (nBins - 1);
-            
-            // allocate memory for result
-            double[] range = new double[] {minValue, maxValue};
-
-            this.xData = new double[nBins];
-            for (int i = 0; i < nBins; i++)
-            {
-                xData[i] = range[0] + i * binWidth;
-            }
-            
-            // allocate memory for result
-            this.yData = new int[1][];
-            this.yData[0] = Histograms.histogramScalar(array, range, nBins);
-            
-            this.channelNames = new String[] {"Intensity"};
-        }
-        
-        private void computeHistogramData_RGB8(RGB8Array array)
-        {
-            this.xData = new double[256];
-            for (int i = 0; i < 256; i++)
-            {
-                xData[i] = i;
-            }
-            this.yData = Histograms.histogramRGB8((RGB8Array) array);
-            
-            this.channelNames = new String[] {"Red", "Green", "Blue"};
-        }
-
-        private void computeHistogramData_RGB16(RGB16Array array)
-        { 
-            int[][] histo = Histograms.histogramRGB16((RGB16Array) array);
-            
-            this.xData = convertToDouble(histo[0]);
-            this.yData = new int[3][];
-            for (int i = 0; i < 3; i++)
-            {
-                this.yData[i] = histo[i+1];
-            }
-            this.channelNames = new String[] {"Red", "Green", "Blue"};
-        }
-
-        /**
-         * Check if it is possible to update bounds of the histogram. Bounds can
-         * be update for scalar array that are not instances of UInt8Array.
-         */
-        public boolean canUpdateBounds()
-        {
-            if (this.refImage.getData() instanceof UInt8Array) 
-            {
-                return false;
-            }
-            return this.refImage.isScalarImage();
-        }
     }
 
     /**
@@ -255,7 +136,13 @@ public class ImageHistogram implements FramePlugin
      */
     class ImageHistogramDisplayFrame extends ImagoFrame
     {
-        Histogram histogram;
+        /** the reference image, for retrieving meta-data and eventually recomputing the histogram */
+        Image refImage;
+        
+        HistogramSettings settings;
+        
+        Histogram.Result[] histograms;
+        String[] channelNames;
         
         boolean logScale = false;
         boolean hideBackground = false;
@@ -273,16 +160,42 @@ public class ImageHistogram implements FramePlugin
         JSpinner maxValueSpinner;
         JSpinner nBinsSpinner;
         
-        public ImageHistogramDisplayFrame(ImagoFrame parentFrame, String name, Histogram histogram) 
+        public ImageHistogramDisplayFrame(ImagoFrame parentFrame, String name, Image image, HistogramSettings settings) 
         {
             super(parentFrame, name);
             
-            this.histogram = histogram;
+            this.refImage = image;
+            this.settings = settings;
+            this.channelNames = computeChannelNames(image);
+            
+            updateHistogram();
             
             createChart();
             
             setupWidgets();
             setupLayout();
+        }
+        
+        private String[] computeChannelNames(Image image)
+        {
+            return switch (image.getData())
+            {
+                case UInt8Array array2 -> new String[] { "Intensity" };
+                case ScalarArray<?> array2 -> new String[] { "Intensity" };
+                case RGB8Array array2 -> new String[] { "Red", "Green", "Blue" };
+                case RGB16Array array2 -> new String[] { "Red", "Green", "Blue" };
+                case VectorArray<?,?> array2 ->
+                {
+                    int nc = array2.channelCount();
+                    String[] res = new String[nc];
+                    for (int c =  0; c < nc; c++)
+                    {
+                        res[c] = "C" + c;
+                    }
+                    yield res;
+                }
+                default -> throw new IllegalArgumentException("Unexpected value: " + image.getData().getClass());
+            };
         }
         
         private void setupWidgets()
@@ -321,28 +234,28 @@ public class ImageHistogram implements FramePlugin
                 }
             });
 
-            if (this.histogram.canUpdateBounds())
+            if (canUpdateBounds())
             {
-                this.histogram.computeDefaultValueRange();
+                this.settings.computeDefaultValueRange(this.refImage);
                 
-                this.minValueSpinner = new JSpinner(new SpinnerNumberModel(this.histogram.minValue, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 1.0));
+                this.minValueSpinner = new JSpinner(new SpinnerNumberModel(this.settings.minValue, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 1.0));
                 getTextField(this.minValueSpinner).setColumns(8);
                 this.minValueSpinner.addChangeListener(evt -> {
-                    this.histogram.minValue = ((SpinnerNumberModel) minValueSpinner.getModel()).getNumber().doubleValue();
-                    this.histogram.update();
+                    this.settings.minValue = ((SpinnerNumberModel) minValueSpinner.getModel()).getNumber().doubleValue();
+                    this.updateHistogram();
                     updateDisplay();
                 });
-                this.maxValueSpinner = new JSpinner(new SpinnerNumberModel(this.histogram.maxValue, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 1.0));
+                this.maxValueSpinner = new JSpinner(new SpinnerNumberModel(this.settings.maxValue, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 1.0));
                 getTextField(this.maxValueSpinner).setColumns(8);
                 this.maxValueSpinner.addChangeListener(evt -> {
-                    this.histogram.maxValue = ((SpinnerNumberModel) maxValueSpinner.getModel()).getNumber().doubleValue();
-                    this.histogram.update();
+                    this.settings.maxValue = ((SpinnerNumberModel) maxValueSpinner.getModel()).getNumber().doubleValue();
+                    this.updateHistogram();
                     updateDisplay();
                 });
-                this.nBinsSpinner = new JSpinner(new SpinnerNumberModel(this.histogram.nBins, 10, 1024, 1));
+                this.nBinsSpinner = new JSpinner(new SpinnerNumberModel(this.settings.nBins, 10, 1024, 1));
                 this.nBinsSpinner.addChangeListener(evt -> {
-                    this.histogram.nBins = ((SpinnerNumberModel) nBinsSpinner.getModel()).getNumber().intValue();
-                    this.histogram.update();
+                    this.settings.nBins = ((SpinnerNumberModel) nBinsSpinner.getModel()).getNumber().intValue();
+                    this.updateHistogram();
                     updateDisplay();
                 });
             }
@@ -363,7 +276,7 @@ public class ImageHistogram implements FramePlugin
             controlPanel.add(hideBackgroundCheckBox);
             controlPanel.add(showLegendCheckBox);
             
-            if (this.histogram.canUpdateBounds())
+            if (canUpdateBounds())
             {
                 controlPanel.add(new JLabel("Min:"));
                 controlPanel.add(minValueSpinner);
@@ -387,7 +300,7 @@ public class ImageHistogram implements FramePlugin
         
         private void createChart()
         {
-            int nChannels = this.histogram.yData.length;
+            int nChannels = this.histograms.length;
 
             // Create Chart
             this.chart = new XYChartBuilder()
@@ -405,8 +318,9 @@ public class ImageHistogram implements FramePlugin
             XYSeries[] series  = new XYSeries[nChannels];
             for (int c = 0; c < nChannels; c++)
             {
-                double[] data = convertToDouble(this.histogram.yData[c]);
-                series[c] = chart.addSeries(this.histogram.channelNames[c], this.histogram.xData, data);
+                Histogram.Result histo = this.histograms[c];
+                double[] data = convertToDouble(histo.counts());
+                series[c] = chart.addSeries(channelNames[c], histo.binCenters(), data);
                 series[c].setMarker(SeriesMarkers.NONE);
             }
 
@@ -419,16 +333,53 @@ public class ImageHistogram implements FramePlugin
             }
         }
         
+        private void updateHistogram()
+        {
+//            histogramData = settings.update();
+            Array<?> array = this.refImage.getData();
+            
+            histograms = switch(array)
+            {
+                case UInt8Array array2  -> computeHistogramData_UInt8(array2);
+                case RGB8Array array2   -> computeHistogramData_RGB8(array2);
+                case RGB16Array array2  -> computeHistogramData_RGB16(array2);
+                case ScalarArray<?> array2 -> computeHistogramData_Scalar(array2);
+                default -> throw new RuntimeException(
+                        "Unable to compute histogram for array class: " + array.getClass());
+            };
+        }
+        
+        private Histogram.Result[] computeHistogramData_UInt8(UInt8Array array)
+        {
+            return new Histogram.Result[] {ImageHistograms.histogramUInt8(array)};
+        }
+        
+        private Histogram.Result[] computeHistogramData_Scalar(ScalarArray<?> array)
+        {
+            Histogram algo = new Histogram(settings.imageValueRange, settings.nBins);
+            return new Histogram.Result[] {algo.process(array)};
+        }
+        
+        private Histogram.Result[] computeHistogramData_RGB8(RGB8Array array)
+        {
+            return ImageHistograms.histogramsRGB8(array);
+        }
+
+        private Histogram.Result[] computeHistogramData_RGB16(RGB16Array array)
+        { 
+            return ImageHistograms.histogramsRGB16(array);
+        }
+
         public void updateDisplay()
         {
             Map<String, XYSeries> map = chart.getSeriesMap();
-            for (int c = 0; c < this.histogram.yData.length; c++)
+            for (int c = 0; c < this.histograms.length; c++)
             {
-                String name = this.histogram.channelNames[c];
+                String name = this.channelNames[c];
                 XYSeries series = map.get(name);
                 
                 // check log scale
-                double[] data = convertToDouble(this.histogram.yData[c]);
+                double[] data = convertToDouble(this.histograms[c].counts());
                 if (logScale)
                 {
                     for (int i = 0; i < data.length; i++)
@@ -445,14 +396,27 @@ public class ImageHistogram implements FramePlugin
                 // check display of histogram bound bins
                 if (hideBackground)
                 {
-                    data[0] = 0;
-                    data[data.length-1] = 0;
+                    data[0] = Double.NaN;
+                    data[data.length-1] = Double.NaN;
                 }
                 
-                series.replaceData(this.histogram.xData, data, null);
+                series.replaceData(this.histograms[c].binCenters(), data, null);
             }
             
             this.chartPanel.repaint();
+        }
+        
+        /**
+         * Check if it is possible to update bounds of the histogram. Bounds can
+         * be update for scalar array that are not instances of UInt8Array.
+         */
+        public boolean canUpdateBounds()
+        {
+            if (this.refImage.getData() instanceof UInt8Array)
+            {
+                return false;
+            }
+            return this.refImage.isScalarImage();
         }
     }
 
