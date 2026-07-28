3078ba1ffb9fe64e32952647e80375f7c7e6b12e
David Legland <>
2026-07-28 13:42:14 +0200

    propagate change in DelimitedTableReader

--- src/main/java/imago/table/plugins/file/OpenTable.java
+++ src/main/java/imago/table/plugins/file/OpenTable.java
@@ -143,7 +143,7 @@ public class OpenTable implements FramePlugin
         dlg.addCheckBox("Header", true);
         dlg.addNumericField("Skip lines", 0, 0);
         dlg.addCheckBox("Read Row Names", false);
-        dlg.addCheckBox("Ignore Double-Quotes", true);
+        dlg.addCheckBox("Parse Double-Quotes", false);
         
         // wait for user input
         dlg.showDialog();
@@ -157,7 +157,7 @@ public class OpenTable implements FramePlugin
                 .setReadHeader(dlg.getNextBoolean())
                 .setSkipLines((int) dlg.getNextNumber())
                 .setReadRowNames(dlg.getNextBoolean())
-                .setIgnoreQuotes(dlg.getNextBoolean());
+                .setParseDoubleQuotes(dlg.getNextBoolean());
         reader.addAlgoListener(frame);
         
         // try reading the table
